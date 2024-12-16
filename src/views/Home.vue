0b048f89760a9ed1d31690e8ff460fbf627ad6c9
Enzo Damascena <enzo.damascena@defensoria.mg.def.br>
2024-12-16 12:50:00 -0300

    refactor: remove HelloWorld component and update Home view with microfrontend link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
-        <h1>
-            HOME CONTAINER-APP
-        </h1>
-    </div>
+  <div>
+    <h1>HOME CONTAINER-APP</h1>
+    <!-- Adicionando o link para a rota do microfrontend -->
+    <router-link to="/vue3">Ir para o Microfrontend Vue 3</router-link>
+  </div>
 </template>
 
 <script>
@@ -11,3 +11,15 @@ export default {
   name: 'App',
 };
 </script>
+
+<style>
+/* Estilo opcional para melhorar a aparência do link */
+router-link {
+  text-decoration: none;
+  color: blue;
+  font-weight: bold;
+}
+router-link:hover {
+  text-decoration: underline;
+}
+</style>
